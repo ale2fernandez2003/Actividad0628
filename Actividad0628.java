ce734b0875b41ef59793cf626fdacd56027c1fbe
Alejandro <ale2fernandez2003@gmail.com>
2024-05-07 13:05:59 +0200

    primer commit

---
+++ Actividad0628.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Actividad0628 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Actividad0628
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Actividad0628
+     */
+    public Actividad0628()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int jugandoALosDardos(double numero, double valor)
+    {
+        int num = 0;
+        return num;
+    }
+}
